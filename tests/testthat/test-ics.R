917c086e1976581ded6a472f4e38bafae8760743
Kai Nehler <nehler@psych.uni-frankfurt.de>
2025-06-05 15:28:23 +0200

    Add first test functions.

---
+++ tests/testthat/test-ics.R
@@ -0,0 +1,21 @@
+test_that("sample_size_full_works", {
+  expect_equal(calculate_sample_size(data = mantar_dummy_full), rep(400, 8))
+})
+
+test_that("sample_size_mis_works", {
+  expect_equal(calculate_sample_size(data = mantar_dummy_mis, n_calc = "TOtal"), rep(600, 8))
+})
+
+test_that("error_for_different_ncalc", {
+  expect_error(
+    calculate_sample_size(data = mantar_dummy_mis, n_calc = "abc"),
+    class = "error"
+  )
+})
+
+test_that("ic_calculated as number",{
+  expect_equal(
+    class(reg_ic_calc(resid_var = 1, n = 400, n_preds = 2, k = "log(n)")),
+    "numeric"
+  )
+})
--- tests/testthat/test-matrix_regression.R
+++ tests/testthat/test-matrix_regression.R
@@ -7,7 +7,6 @@ test_that("matrix regression returns correct dimensions for beta weights", {
   expect_equal(length(matrix_regression(mat = example_matrix, dep_ind = 1, pred_ind = NULL)$beta), 0)
 })
 
-
 test_that("matrix regression returns correct dimensions for residual variance", {
   expect_equal(length(matrix_regression(mat = example_matrix, dep_ind = 1, pred_ind = c(2,3))$resid_var), 1)
   expect_equal(length(matrix_regression(mat = example_matrix, dep_ind = 1, pred_ind = NULL)$resid_var), 1)
